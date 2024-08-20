fef23abd6bdff6570b973950970beeddd2219c49
Vitor Martini <vitor-martini@outlook.com>
2024-08-19 21:31:43 -0300

    feat: join class interface

--- src/app/student/class/page.jsx
+++ src/app/student/class/page.jsx
@@ -1,46 +1,83 @@
 "use client";
 import { useEffect, useState } from "react";
-import { Container, ContentContainer } from "./styles";
+import { 
+  Container, 
+  ContentContainer, 
+  ModalContent, 
+  ModalButtonsContent, 
+  AccessKeyContainer,
+  AccessKeyWrapper} from "./styles";
 import { Header } from "@/components/Header";
 import { Input } from "@/components/Input";
+import { Button } from "@/components/Button";
 import { ClassCard } from "@/components/ClassCard";
 import { api } from "@/lib/api";
 import { useTheme } from "styled-components";
 import { CiSearch } from "react-icons/ci";
 import { useRouter } from "next/navigation";
 import { toast } from "react-toastify";
+import Modal from "react-modal";
+import { FaRegPaste } from "react-icons/fa6";
 
 const ClassDashboard = () => {
   const router = useRouter();
   const theme = useTheme();
   const [classes, setClasses] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const [search, setSearch] = useState("");
+  const [accessKey, setAccessKey] = useState("");
   const newClass = {
     id: 0,
-    name: "Adicionar nova turma"
+    name: "Ingressar em nova turma"
   };
 
   function handleNew() {
-    router.push("/teacher/class/new");
+    setIsModalOpen(true);
+  }
+
+  async function handlePasteAccessKey() {
+    try {
+      const text = await navigator.clipboard.readText();
+      setAccessKey(text);
+    } catch (error) {
+      console.error("Falha ao acessar a área de transferência: ", error);
+    }
+  }
+
+  async function joinClass() {
+    try {
+      await api.post("/classes/join", {
+        accessKey
+      });
+      toast.success("Você ingressou na nova turma!");
+      await fetchClasses();
+      closeModal();
+    } catch (error) {
+      console.log(error);
+      const errorMessage = error.response?.data?.message;
+      if (errorMessage) {
+        toast.error(errorMessage);
+      } else {
+        toast.error("Não foi possível ingressar");
+      }
+    }
+  }
+
+  async function fetchClasses() {
+    const response = await api.get(`/classes?name=${search}`);
+    const classes = response?.data?.classroom;
+    setClasses(classes);
+  }
+
+  function closeModal() {
+    setAccessKey("");
+    setIsModalOpen(false);
   }
 
   useEffect(() => {
-    async function fetchClasses() {
-      const response = await api.get(`/classes?name=${search}`);
-      const classes = response?.data?.classroom;
-      setClasses(classes);
-    }
-
+    Modal.setAppElement("#__next");
     fetchClasses();
   }, [search]);
-
-  useEffect(() => {
-    const message = sessionStorage.getItem("messageStorage");
-    if (message) {
-      toast.success(message);
-      sessionStorage.removeItem("messageStorage"); 
-    }
-  }, []);
 
   return (
     <Container>
@@ -70,6 +107,47 @@ const ClassDashboard = () => {
           ))
         }
       </ContentContainer>
+      {isModalOpen && (
+        <Modal
+          isOpen={isModalOpen}
+          onRequestClose={closeModal}
+          contentLabel="Join new Class"
+          style={{
+            content: {
+              top: "50%",
+              left: "50%",
+              right: "auto",
+              bottom: "auto",
+              marginRight: "-50%",
+              transform: "translate(-50%, -50%)",
+              boxShadow: "0 4px 8px rgba(0,0,0,0.5)",
+              borderRadius: "20px", 
+              backgroundColor: "#FFF"
+            },
+          }}
+        >
+          <ModalContent>
+            <h2>Informe a chave de acesso para ingressar em uma nova turma!</h2>
+            <AccessKeyContainer>
+                <AccessKeyWrapper>
+                  <Input
+                    placeholder={"Chave de acesso"}
+                    width={"100%"}
+                    bgColor={"transparent"}
+                    border={"none"}
+                    onChange={(e => setAccessKey(e.target.value))}
+                    value={accessKey}
+                  />
+                  <FaRegPaste size={60} onClick={handlePasteAccessKey} />
+                </AccessKeyWrapper>
+              </AccessKeyContainer>
+            <ModalButtonsContent>
+              <Button title={"Ingressar"} width={"100%"} onClick={joinClass} />
+              <Button title={"Cancelar"} width={"100%"} onClick={closeModal} />
+            </ModalButtonsContent>
+          </ModalContent>
+        </Modal>
+      )}
     </Container>
   );
 };
--- src/app/student/class/styles.js
+++ src/app/student/class/styles.js
@@ -16,3 +16,61 @@ export const ContentContainer = styled.div`
   justify-content: center;
   flex-wrap: wrap; 
 `;
+
+export const AccessKeyContainer = styled.div`
+  > label {
+    padding: 1.6rem;
+  }
+`;
+
+export const AccessKeyWrapper = styled.div`
+  border: 2px solid ${({ theme }) => theme.COLORS.PURPLE};
+  overflow: hidden;
+  background-color: ${({ theme }) => theme.COLORS.WHITE};
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-radius: 20px;
+  gap: 0.8rem;
+  width: 100%;
+  margin-top: 0.8rem;
+
+  > svg {
+    color: ${({ theme }) => theme.COLORS.LIGHT};
+    background-color: ${({ theme }) => theme.COLORS.PURPLE};
+    padding: 0 1.2rem;
+    cursor: pointer;
+  }
+
+  > div {
+    width: 100%;
+  }
+`;
+
+export const ModalContent = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 2rem;
+
+  > div {
+    width: 100%;
+  }
+`;
+
+export const ModalButtonsContent = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 2rem;
+  width: 100%;
+
+  > button {
+    box-shadow: 0 4px 8px rgba(0,0,0,0.5);
+
+    &:first-child {
+      background-color: ${({ theme }) => theme.COLORS.GREEN}
+    }
+  }
+`;
